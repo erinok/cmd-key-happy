26cf8c7fa1f8eb14d61484de8eb8bf13ae35c31d
Erin Keenan <erinok@gmail.com>
2018-05-01 12:54:01 -0700

    cmake output build files

---
+++ cpp/xcode-build/CMakeFiles/cmd-key-happy.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/erin/3p/cmd-key-happy/cpp/CmdKeyHappy.mm" "/Users/erin/3p/cmd-key-happy/cpp/xcode-build/CMakeFiles/cmd-key-happy.dir/CmdKeyHappy.mm.o"
+  "/Users/erin/3p/cmd-key-happy/cpp/EventTap.mm" "/Users/erin/3p/cmd-key-happy/cpp/xcode-build/CMakeFiles/cmd-key-happy.dir/EventTap.mm.o"
+  "/Users/erin/3p/cmd-key-happy/cpp/KeyEvent.mm" "/Users/erin/3p/cmd-key-happy/cpp/xcode-build/CMakeFiles/cmd-key-happy.dir/KeyEvent.mm.o"
+  "/Users/erin/3p/cmd-key-happy/cpp/KeySeq.mm" "/Users/erin/3p/cmd-key-happy/cpp/xcode-build/CMakeFiles/cmd-key-happy.dir/KeySeq.mm.o"
+  "/Users/erin/3p/cmd-key-happy/cpp/ProcessInfo.mm" "/Users/erin/3p/cmd-key-happy/cpp/xcode-build/CMakeFiles/cmd-key-happy.dir/ProcessInfo.mm.o"
+  "/Users/erin/3p/cmd-key-happy/cpp/ProcessList.mm" "/Users/erin/3p/cmd-key-happy/cpp/xcode-build/CMakeFiles/cmd-key-happy.dir/ProcessList.mm.o"
+  "/Users/erin/3p/cmd-key-happy/cpp/main.mm" "/Users/erin/3p/cmd-key-happy/cpp/xcode-build/CMakeFiles/cmd-key-happy.dir/main.mm.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/xcode-build/CMakeFiles/cmd-key-happy.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmd-key-happy.dir/CmdKeyHappy.mm.o"
+  "CMakeFiles/cmd-key-happy.dir/EventTap.mm.o"
+  "CMakeFiles/cmd-key-happy.dir/KeyEvent.mm.o"
+  "CMakeFiles/cmd-key-happy.dir/ProcessInfo.mm.o"
+  "CMakeFiles/cmd-key-happy.dir/ProcessList.mm.o"
+  "CMakeFiles/cmd-key-happy.dir/main.mm.o"
+  "CMakeFiles/cmd-key-happy.dir/KeySeq.mm.o"
+  "cmd-key-happy.pdb"
+  "cmd-key-happy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmd-key-happy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
